3866dd393216200117dd556db88b6195a65d5ed4
jillianfairchild <fairchild.jillian@gmail.com>
2016-10-04 20:04:48 -0400

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,20 @@ var sassOptions = {
 	outputStyle: 'expanded'
 };
 
+var sassSources = './scss/**/*.scss';
+var sassOutput = './app/css';
+var htmlSource = 'app/**/*.html';
+
 // gulp.task('test', function(){
 // 	console.log('testing 1 2 3')	
 // });
 
 gulp.task('sass', function(){
-	return gulp.src('scss/styles.scss')
+	return gulp.src(sassSources)
 	.pipe(sourcemaps.init())
 	.pipe(sass(sassOptions).on('error', sass.logError))
 	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('app/css'))	
+	.pipe(gulp.dest(sassOutput))	
 	.pipe(browserSync.stream())
 });
 
@@ -29,6 +33,9 @@ gulp.task('serve', ['sass'], function(){
 
 
 
-	gulp.watch('scss/**/*.scss', ['sass'])
-	gulp.watch('app/**/*.html').on('change', browserSync.reload);
+	gulp.watch(sassSources, ['sass'])
+	gulp.watch(htmlSource).on('change', browserSync.reload);
 });
+
+
+gulp.task('default', ['serve']);
